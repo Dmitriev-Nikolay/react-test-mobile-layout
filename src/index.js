ce397da669cb6c550fe4a84d7b4300078a0e28d7
Nikolay <dmitriev9229@mail.ru>
2021-08-03 18:27:13 +0300

    bug fix and optimization # 5.7
    cache rsw

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
 
 import './scss/app.scss';
 import App from './App';
@@ -10,3 +11,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+registerServiceWorker();
