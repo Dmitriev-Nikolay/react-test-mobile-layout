2380627b993eb6510c25e733e38329a61370bf4b
Nikolay <dmitriev9229@mail.ru>
2021-08-03 23:00:44 +0300

    Updates

---
+++ static/js/3.a88a48bd.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[3],{22:function(c,e,s){"use strict";s.r(e);s(1);var t=s(5),n=s(0);e.default=function(){return Object(n.jsx)("div",{className:"content",children:Object(n.jsxs)("div",{className:"container",children:[Object(n.jsx)(t.j,{}),Object(n.jsx)(t.c,{}),Object(n.jsx)(t.e,{})]})})}}}]);
+//# sourceMappingURL=3.a88a48bd.chunk.js.map
--- static/js/3.bd006f4c.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[3],{22:function(c,s,e){"use strict";e.r(s);e(1);var t=e(5),n=e(0);s.default=function(){return Object(n.jsx)("div",{className:"content",children:Object(n.jsxs)("div",{className:"container",children:[Object(n.jsx)(t.k,{}),Object(n.jsx)(t.c,{}),Object(n.jsx)(t.f,{})]})})}}}]);
-//# sourceMappingURL=3.bd006f4c.chunk.js.map
